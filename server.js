c5d9b76a350a8004a73298559415f72d16b468c7
lianjian zhou <1126185240@qq.com>
2016-11-21 22:36:43 +0800

    h

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = express()
 var directory = process.argv[2] || '/'
 server.use(express.static(directory))
 server.listen(3000, function(){
-    console.log('server runing at: http://localhost:3000')
+    console.log('server runing at: http://localhost:3000 on directory: ./' + directory)
 })
 
 server.get('/favicon.ico', function(req, res){
